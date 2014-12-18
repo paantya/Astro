f7b4908e071ee1cb51796be818a6e1e36e2c1d03
Patshin Anton <paantya@gmail.com>
2014-12-18 12:49:46 +0300

    Edit Errors Not accept output

--- 3s/programming/podmatrix.cpp
+++ 3s/programming/podmatrix.cpp
@@ -1,31 +1,62 @@
 #include <iostream>
-int main()
-{
-  omp_lock_t lockM,lockT;
-  omp_init_lock(&lockT);
+#include <omp.h> 
+using namespace std;
+
+int main() {
+  omp_lock_t lockM;
   omp_init_lock(&lockM);
+  
+  int n,m;
+  int a[1000][1000]; 
   int x0,y0,x1,y1;
+  double M;
+  
+  cin >> n >> m;
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < m; ++j)
+      cin >> a[i][j];
+  
   #pragma omp parallel for
-  for (int i = 0; i < 2; ++i)
-  {
-    std::cout << "Hello World!" << i << std::endl;
-  }
-  
-  if (t>M){
-    omp_set_lock(&lockM);
-    if (t>M){
-    //
+  for (int i = 0; i < n; ++i) {
+    int t = 0;
+    for (int j = 0 ; j < m; ++j) {
+      for (int k = j; k < m; ++k) {
+        do {
+          t += a[i][k];
+        } while ((++k < m) && (t > 0));
+        int w = 0;
+        for (int l = j; j < k; ++l) {
+          for (int u = i; u < n; ++u) {
+            w +=a[u][l];
+            if (w > M) {
+              omp_set_lock(&lockM);
+              if (w > M){
+                M = w;
+                y0 = i;
+                x0 = j;
+                y1 = u;
+                x1 = l;
+                //
+              }
+              omp_unset_lock(&lockM);
+            }                    
+          }
+          if (w <= 0) break;
+        }
+        t = 0;
+        --k;
+      }
+      t = 0;
     }
-    omp_unset_lock(&lockM);
-  }
-  if (t<=0){
-    omp_set_lock(&lockT);
-    //
-    omp_unset_lock(&lockT);
   }
   
-  omp_destroy_lock(&lockM);
-  omp_destroy_lock(&lockT);
+  cout << x0 <<", "<< y0 << endl << x1 <<", "<< y1 << endl;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j)
+      cout << a[i][j];
+    cout << endl;
+  } 
   
+  omp_destroy_lock(&lockM); 
   return 0;
 }
