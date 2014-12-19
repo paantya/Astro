8ec35c99fecdc827697e5a7969cf349d16c535f5
Patshin Anton <paantya@gmail.com>
2014-12-19 03:49:46 +0300

    Update 1.2 1. код поиска подматрицы вынесет в отдельную функцию. 2. Переписано объявление массива a[N,M]. 3. Bug Fix. Корректно работает на нулевых и отрицательных массивах. 4. Code Review.

--- 3s/programming/podmatrix.cpp
+++ 3s/programming/podmatrix.cpp
@@ -1,66 +1,71 @@
 #include <iostream>
 #include <omp.h> 
-using namespace std;
 #include <limits>
+using namespace std;
 
+long lmin = numeric_limits<long>::min();
+int n,m;
 
-int main() {
-  omp_lock_t lockM;
-  omp_init_lock(&lockM);
-  long lmin = numeric_limits<long>::min();
-  
-  int n,m;
-  int x0=-1,y0=-1,x1=-1,y1=-1;
+void max_podmatrix(int ** a, int &x0, int &y0, int &x1, int &y1) {
   long M = lmin;
-  cin >> n >> m;
-  int a[n][m]; 
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < m; ++j)
-      cin >> a[i][j];
-  
-  #pragma omp parallel for
-  for (int i = 0; i < n; ++i) {
+  #pragma omp parallel for shared(M,x0,y0,x1,y1)
+  for (size_t i = 0; i < n; ++i) {
     long t;
-    for (int j = 0 ; j < m; ++j) {
-      for (int k = j; k < m; ++k) {
+    for (size_t j = 0 ; j < m; ++j) {
+      for (size_t k = j; k < m; ++k) {
         t = lmin;
         do {
           t += a[i][k];
           ++k;
         } while ((k < m) && (t > 0));        
-        long w = lmin;        
-        for (int q = j; q < k; ++q) {
-          for (int u = i; u < n; ++u) {            
-            for (int l = j; l <=q; ++l) 
+        long w;        
+        for (size_t q = j; q < k; ++q) {
+          w = 0;
+          for (size_t u = i; u < n; ++u) {            
+            for (size_t l = j; l <=q; ++l) 
               w +=a[u][l];                   
             if (w > M) {
-                omp_set_lock(&lockM);
+                #pragma omp critical
                 if (w > M){
                   M = w;
                   y0 = i;
                   x0 = j;
                   y1 = u;
                   x1 = q;
-                  //cout << "chang " << M << ':' << x0 <<"."<< y0 << ' ' << x1 << '.' << y1 << endl;
+                  //cout << "chang " << M << ':' << x0 << "." << y0 << ' ' \
+                    << x1 << '.' << y1 << endl;
                 }
-                omp_unset_lock(&lockM);
               } 
-            if (w <= 0) { w = 0; break; }
+            if (w <= 0) break;
           }
-          w = 0;
         }
         --k;
       }
     }
   }
+}
+
+int main() {
+  int x0=-1,y0=-1,x1=-1,y1=-1;
+  cin >> n >> m;
+  int ** a = new int *[n];
+  a[0] = new int[n * m];
+  for ( size_t i = 1; i != n; ++i)
+    a[i] = a[i - 1] + m;
   
-  cout << "y(n), x(m):\nbegin: " << y0 <<", "<< x0 << "\nend:   " << y1 <<", "<< x1 << "\nMatrix:\n";
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j)
+  for (size_t i = 0; i < n; ++i)
+    for (size_t j = 0; j < m; ++j)
+      cin >> a[i][j];
+  
+  max_podmatrix(a, x0, y0, x1, y1);
+  
+  cout << "y(n), x(m):\nbegin: " << y0 << ", " << x0 << "\nend:   " << y1 \
+    << ", " << x1 << "\nMatrix:\n";
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j)
       cout << a[i][j] << ' ';
     cout << endl;
   } 
   
-  omp_destroy_lock(&lockM); 
   return 0;
 }
